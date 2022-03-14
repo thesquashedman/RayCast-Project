8650762c28990e20fffff5c1f81764336bf2c40b
Pavel Peev <76268971+thesquashedman@users.noreply.github.com>
2022-03-13 17:11:08 -0700

    rc sprite renderable now works, just needs draw logic

--- src/engine/renderables/rc_sprite_renderable.js
+++ src/engine/renderables/rc_sprite_renderable.js
@@ -12,70 +12,80 @@ class RCSpriteRenderable extends SpriteRenderable{
     draw(camera, position)
     {
         let cameraPos = camera.getRayCasterPos();
+        let resolution = Math.floor(camera.getResolution());
         let angle = Math.atan((cameraPos[1] - position[1])/(cameraPos[0] - position[0]));
         let distance = Math.sqrt(Math.pow(cameraPos[1] - position[1], 2) + Math.pow(cameraPos[0] - position[0], 2));
         if(cameraPos[0] < position[0] && cameraPos[1] > position[1])
         {
+            
             angle += Math.PI;
         }
         else if(cameraPos[0] < position[0] && cameraPos[1] < position[1])
         {
             angle -= Math.PI;
         }
-        if(camera.getFOV() >= 2 * Math.PI)
+        if(angle < 0)
         {
-            console.log("SpriteRenderable is not supported above FOVs of 2pi");
+            angle += Math.PI;
         }
         else{
-            console.log((360 * angle)/(2 * Math.PI));
-            let lowerBound = camera.getRayCasterAngle() - (1/2) * camera.getFOV();
-            console.log((360 * lowerBound)/(2 * Math.PI) + " Lower");
-            let upperBound = camera.getRayCasterAngle() + (1/2) * camera.getFOV();
-            console.log((360 * upperBound)/(2 * Math.PI) + " Upper");
-            let inverseLower = false;
-            let inverseUpper = false;
-            let tempAngle = angle;
-            if(tempAngle < 0)
-            {
-                tempAngle += 2 * Math.PI;
-            }
-            if(lowerBound < 0)
-            {
-                lowerBound += Math.PI * 2;
-                inverseLower = true;
-            }
-            if(upperBound > Math.PI * 2)
-            {
-                upperBound -= Math.PI * 2;
-                inverseUpper = true;
-            }
-            if(tempAngle < upperBound && angle > lowerBound)
-            {
-                console.log("Within");
-                let index = 0;
-                if(inverseLower || inverseUpper)
-                {
-                    let offset = 2* Math.PI - lowerBound;
-                    index = (camera.getResolution() - 2) - Math.floor((offset + angle)/(upperBound + offset) * (camera.getResolution() - 1));
+            angle -= Math.PI;
+        }
+        console.log((360 * angle)/(2 * Math.PI));
+        let lowerBound = camera.getRayCasterAngle() - (1/2) * camera.getFOV();
+        let upperBound = camera.getRayCasterAngle() + (1/2) * camera.getFOV();
+        let inverseLower = false;
+        let inverseUpper = false;
+        let tempAngle = angle;
+        if(tempAngle < 0)
+        {
+            tempAngle += 2 * Math.PI;
+        }
+        if(lowerBound < 0)
+        {
+            lowerBound += Math.PI * 2;
+            inverseLower = true;
+        }
+        if(upperBound > Math.PI * 2)
+        {
+            //upperBound -= Math.PI * 2;
+            inverseUpper = true;
+        }
+        let within = false;
+        if(tempAngle < upperBound && tempAngle > lowerBound)
+        {
+            within = true;
+        }
+        //console.log("Within");
+        let index = 0;
+        if(inverseLower || inverseUpper)
+        {
+            let offset = 2* Math.PI - lowerBound;
+            index = (resolution - 2) - Math.floor((offset + angle)/(upperBound + offset) * (resolution - 1));
 
 
-                }
-                else{
-                    index = (camera.getResolution() - 2) - Math.floor((tempAngle - lowerBound)/(upperBound - lowerBound)) * (camera.getResolution() - 1);
-                    
-                }
-                if(camera.getRaycastLengths[index] > distance && camera.getRaycastLengths[index + 1] > distance)
-                {
-                    console.log("Visible");
-                }
-                    
+        }
+        else{
+            index = (resolution - 2) - Math.floor((tempAngle - lowerBound)/(upperBound - lowerBound) * (resolution - 1));
+            
+        }
+        console.log(index + " Index");
+        if(index >= 0 && index < resolution - 1)
+        {
+            console.log("within");
+            if(camera.getRaycastLengths()[index] > distance && camera.getRaycastLengths()[index + 1] > distance)
+            {
+                console.log("Visible");
+                //super.draw(camera);
             }
         }
+        
+    
         
         
         
         //this.mXform.getSize()
-        //super.draw(camera);
+        
     }
 
 }
